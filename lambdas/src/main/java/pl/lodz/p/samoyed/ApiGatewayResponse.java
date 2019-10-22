d890e8e5f46d26529352fa5b7522f136d364410c
Marcin Dłubakowski <m.dlubakowski@gmail.com>
2019-10-22 18:30:15 +0200

    Added base maven project for backend on lambdas with an example request handler

---
+++ lambdas/src/main/java/pl/lodz/p/samoyed/ApiGatewayResponse.java
@@ -0,0 +1,34 @@
+package pl.lodz.p.samoyed;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ApiGatewayResponse {
+
+    public int statusCode;
+    public String body;
+    public Map<String, String> headers;
+    public final boolean isBase64Encoded = false;
+
+    public ApiGatewayResponse() {
+        this.statusCode = 200;
+        this.headers = new TreeMap<String, String>();
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public boolean isBase64Encoded() {
+        return isBase64Encoded;
+    }
+
+}
