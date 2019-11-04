a9ab04172547b87fcfc2d4d48d34dfd217e9bd72
Michael Kidawa <mkidawa1@gmail.com>
2019-11-04 13:08:57 +0100

    404 page added

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import NotFound from "../pages/NotFound/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
