78854349efe485b801ba4961a5cafe56c1ce9fc1
Michael Kidawa <mkidawa1@gmail.com>
2019-11-04 13:00:12 +0100

    Basic routing added, front adapted to React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
-import React, { Component } from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, NavItem, Nav} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./App.css";
+import Routes from './routes/Routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/logo.png';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        Testowy Tekst
-      </div>
-    )
-  }
+
+function App(props) {
+  return (
+    <>
+      <Navbar id="mainNav" bg="light" expand="lg" fluid collapseOnSelect>
+        <div className="App container">
+        <Navbar.Brand>
+          <Link to="/">
+            <img alt="Nasze logo" src={logo} className="img-logo"/>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse >
+          <Nav className="ml-auto" pullRight>
+          <LinkContainer to="/signup">
+              <NavItem>Zarejestruj się</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Zaloguj się</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/tests">
+              <NavItem>Testy</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/team">
+              <NavItem>Zespół</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+        </div> 
+      </Navbar>
+      <Routes />
+    </>
+  );
 }
+
+export default App;
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Home</h1>
+        <p>Oto nasza (pusta) strona startowa</p>
+      </div>
+    </div>
+  );
+}
