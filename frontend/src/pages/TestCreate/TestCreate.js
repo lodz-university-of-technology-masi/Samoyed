c0f0ba49a80f08e059fc5d61c74bf0f0c8d0cb1f
Marcin Dłubakowski <m.dlubakowski@gmail.com>
2019-12-12 09:19:02 +0100

    Modified TestCreate for choice questions

--- frontend/src/pages/TestCreate/TestCreate.js
+++ frontend/src/pages/TestCreate/TestCreate.js
@@ -5,16 +5,46 @@ export default function TestCreate()
     const [title, setTitle] = useState("")
     const [questions, setQuestions] = useState([])
 
+    // Renders block of modifiable questions
     const questionList = questions.map((q, i) => {
+        // Depending on question type, this renders different blocks for typing
+        // answers in
+        let answersBlock;
+        if (q.type === "W") {
+            answersBlock = []
+            for (let m = 0; m < 4; m++) {
+                answersBlock.push(<>
+                    <input className="form-check-input" type="checkbox" 
+                            onChange={e => { changeAnswersCorrect(i, m, e.target.checked) }}
+                            checked={q.answers[m].correct} />
+                    <input className="form-control mr-2" placeholder={"Odpowiedź " + (m+1)}
+                            onChange={e => { changeAnswersComplex(i, m, e.target.value) }}
+                            value={q.answers[m].value} />
+                </>)
+            }
+            answersBlock = (
+                <div className="form-group form-inline row">
+                    {answersBlock}
+                </div>
+            )
+        } else {
+            answersBlock = (
+                <div className="form-group row">
+                    <input className="form-control" placeholder="Odpowiedzi" 
+                            value={q.answers}
+                            onChange={(e) => { changeAnswersSimple(i, e.target.value) }}/>
+                </div>
+            )
+        }
         return (
             <div key={i}>
                 <div className="form-inline row" key={i}>
                     <input className="form-control mr-2 mb-2" placeholder="Pytanie"
                             value={q.content}
-                            onChange={(e) => { changeQuestion(i, "content", e.target.value) }} />
+                            onChange={(e) => { changeContent(i, e.target.value) }} />
                     <select className="form-control mr-2 mb-2"
                             value={q.type}
-                            onChange={(e) => { changeQuestion(i, "type", e.target.value) }}>
+                            onChange={(e) => { changeType(i, e.target.value) }}>
                         <option value="W">Wyboru</option>
                         <option value="O">Otwarte</option>
                         <option value="L">Liczbowe</option>
@@ -24,11 +54,7 @@ export default function TestCreate()
                         Usuń
                     </button>
                 </div>
-                <div className="form-group row">
-                        <input className="form-control" placeholder="Odpowiedzi" 
-                                value={q.answers}
-                                onChange={(e) => { changeQuestion(i, "answers", e.target.value) }}/>
-                </div>
+                {answersBlock}
             </div>
         )
     })
@@ -38,7 +64,12 @@ export default function TestCreate()
         newQuestions.push({
             content: "",
             type: "W",
-            answers: ""
+            answers: [
+                {value:"", correct:true},
+                {value:"", correct:false},
+                {value:"", correct:false},
+                {value:"", correct:false}
+            ]
         })
         setQuestions(newQuestions)
     }
@@ -49,9 +80,44 @@ export default function TestCreate()
         setQuestions(newQuestions)
     }
 
-    function changeQuestion(n, field, value) {
+    // Data handlers
+    function changeContent(n, value) {
         let newQuestions = [...questions]
-        newQuestions[n][field] = value;
+        newQuestions[n].content = value;
+        setQuestions(newQuestions)
+    }
+
+    function changeType(n, value) {
+        let newQuestions = [...questions]
+        newQuestions[n].type = value;
+        if (value === "W") {
+            newQuestions[n].answers = [
+                    {value:"", correct:true},
+                    {value:"", correct:false},
+                    {value:"", correct:false},
+                    {value:"", correct:false}
+                ]
+        } else {
+            newQuestions[n].answers = ""
+        }
+        setQuestions(newQuestions)
+    }
+
+    function changeAnswersSimple(n, value) {
+        let newQuestions = [...questions]
+        newQuestions[n].answers = value;
+        setQuestions(newQuestions)
+    }
+
+    function changeAnswersComplex(n, m, value) {
+        let newQuestions = [...questions]
+        newQuestions[n].answers[m].value = value;
+        setQuestions(newQuestions)
+    }
+
+    function changeAnswersCorrect(n, m, value) {
+        let newQuestions = [...questions]
+        newQuestions[n].answers[m].correct = value;
         setQuestions(newQuestions)
     }
 
@@ -63,7 +129,20 @@ export default function TestCreate()
         let test = {
             title: title,
             questions: questions.map(q => {
-                return {...q, lang: "PL", answers: q.answers.split(";").join("|")}
+                if (q.type === "W") {
+                    // For type W, concat all answers into string "value;correct",
+                    // then concat all answers together with ";"
+                    return {
+                        ...q, 
+                        lang: "PL",
+                        answers: q.answers.map(a => {
+                            return a.value + ";" + a.correct
+                        }).join(";")
+                    }
+                } else {
+                    // For any other type, just concat with ";"
+                    return {...q, lang: "PL", answers: q.answers.split(";").join("|")}
+                }
             })
         }
         console.log(JSON.stringify(test))
