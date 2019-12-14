534bb9f9bb2ab57ad1acdf16fe513336fc00b543
Marcin Dłubakowski <m.dlubakowski@gmail.com>
2019-12-14 20:34:21 +0100

    ApiRequest function now automatically adds Cognito token as header

--- frontend/src/ApiRequest.js
+++ frontend/src/ApiRequest.js
@@ -1,3 +1,5 @@
+import store from './redux/store'
+
 export default function apiRequest(options) {
     let xhr = new XMLHttpRequest()
     xhr.open(
@@ -14,6 +16,13 @@ export default function apiRequest(options) {
             }
         }
     }
+    // Automatically attach Cognito token if present
+    const state = store.getState()
+    if (state.isLogged) {
+        let token = state.tokens.idToken
+        xhr.setRequestHeader("Authorization", "Bearer " + token)
+    }
+    // Send request
     if (options.body) xhr.send(JSON.stringify(options.body))
     else xhr.send(null)
 }
