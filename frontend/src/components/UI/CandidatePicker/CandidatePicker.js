e8ebebf7262f489a46c8fee92ebf3d7c08508269
Michael Kidawa <mkidawa1@gmail.com>
2020-01-09 22:50:54 +0100

    Modal upgraded

--- frontend/src/components/UI/CandidatePicker/CandidatePicker.js
+++ frontend/src/components/UI/CandidatePicker/CandidatePicker.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Button, Col, Row } from "react-bootstrap";
+import './CandidatePicker.css';
 
 const CandidatePicker = props => {
   const { show, setShow, title } = props;
+  const [ picked, setPicked ] = useState(false);
 
   const users = [
     { id: "1", name: "Michał", surname: "Kidawa" },
@@ -10,16 +12,19 @@ const CandidatePicker = props => {
     { id: "3", name: "Krzysztof", surname: "Stępień" }
   ];
 
-  const chooseUser = () => {};
+  const chooseUser = () => {
+      setPicked(true);
+  };
 
   const handleClose = () => {
     setShow(false);
+    setPicked(false);
   };
 
   const renderUsers = () => {
     return users.map(user => {
       return (
-        <Row>
+        <Row onClick={() => chooseUser()} className="candidate__row">
           <Col lg={4}>{user.id}</Col>
           <Col lg={4}>{user.name}</Col>
           <Col lg={4}>{user.surname}</Col>
@@ -29,19 +34,21 @@ const CandidatePicker = props => {
   };
 
   return (
+
     <Modal size="lg" show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Udziel dostępu do testu: "{title}" </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h3>Lista kandydatów:</h3>
+        <h3 className="candidate__header">Lista dostępnych kandydatów:</h3>
         <div>{renderUsers()}</div>
       </Modal.Body>
       <Modal.Footer>
+        <p className="candidate__text">Kandydat: </p>
         <Button variant="secondary" onClick={handleClose}>
           Zamknij
         </Button>
-        <Button variant="primary" onClick={handleClose}>
+        <Button disabled={!picked} variant="primary" onClick={handleClose}>
           Zapisz zmiany
         </Button>
       </Modal.Footer>
